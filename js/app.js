ac796eb5971cce2436c5b1d302675e25a44692e7
Felix_Y <mac@felix.lan>
2016-01-19 01:02:11 +0800

    lesson 6

--- js/app.js
+++ js/app.js
@@ -1,3 +1,15 @@
 window.onload = function() {
   console.log("page loaded");
+
+  makeCartScrollNicely();
+}
+
+
+window.scroll=
+function(){
+  makeCartScrollNicely() ;
+}
+function makeCartScrollNicely() {
+    var cart = document.querySelector('.cart');
+      Ps.initialize(cart);
 }
